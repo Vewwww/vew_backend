2d659a7e3fa1c8e431a745305c52c50c56ce2ea3
Nourin <nourin.osama55@gmail.com>
2023-04-10 04:11:28 +0200

    Error handling

--- app.js
+++ app.js
@@ -1,25 +1,16 @@
-<<<<<<< HEAD
-const express =require('express');
-const app = express();
-
-require("dotenv").config({path:"../../config/.env"});
-
-const connectDB =require('./src/database/dbconnection');
-
-app.listen(process.env.PORT,()=>{
-    connectDB;
-    console.log(`listening on port ${process.env.PORT}`)
-})
-=======
 const express = require('express')
 const { dbConnection } = require('./src/database/dbConnection')
 require("dotenv").config({path:"./config/.env"})
 const app = express()
 const port = process.env.PORT
 const morgan =require("morgan")
+const AppErr = require('./src/utils/AppErr')
 
 app.use(express.json())
 app.use(morgan("dev"))
+app.all('*',(req,res,next)=>{
+    next(new AppErr(`can't find this route: ${req.originalUrl} on server`,404))
+})
+app.use()
 dbConnection();
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
->>>>>>> Nourin
