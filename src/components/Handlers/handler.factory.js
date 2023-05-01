6f1bd3b6123af5edfe79d5556f07ea8e2507249f
Nourin <nourin.osama55@gmail.com>
2023-05-01 17:27:19 +0300

    updates

--- src/components/Handlers/handler.factory.js
+++ src/components/Handlers/handler.factory.js
@@ -6,12 +6,9 @@ const jwt = require('jsonwebtoken');
 exports.signup =(model)=>{
     return catchAsyncErr(async (req, res, next) => {
         const isUser = await model.findOne({ email: req.body.email })
-        if (isUser) {
-            console.log("error here");
-            return next(new AppError("user already exists", 401)); 
-            
-        }
-    
+        if (isUser) 
+            return next(new AppError("user already exists", 401));             
+        
         let User = new model(req.body);
         await User.save();
         res.status(200).json(User);
@@ -22,7 +19,6 @@ exports.signin = (model)=>{
     const user = await model.findOne({ email: req.body.email })
     if (!user || ! await bcrypt.compare(req.body.password, user.password))
         return next(new AppError("incorrect email or password", 401));
-        console.log("error here");
 
 
     let token = jwt.sign({ name: user.name, userId: user._id }, process.env.JWT_KEY);
