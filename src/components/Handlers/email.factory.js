b621a551767f25fddf74675d674326b423d4e0b0
Nourin <nourin.osama55@gmail.com>
2023-05-09 21:43:32 +0300

    emailVerification,serCrud

---
+++ src/components/Handlers/email.factory.js
@@ -0,0 +1,31 @@
+const nodemailer = require("nodemailer");
+const { catchAsyncErr } = require("../../utils/CatchAsyncErr");
+module.exports.sendEmail=async(options)=>{
+    let transporter = nodemailer.createTransport({
+       service:"gmail",
+        auth: {
+          user: "vewww.vew@gmail.com ", // generated ethereal user
+          pass: "cgjnxcjvmxmkuizq", // generated ethereal password
+        },
+      });
+       
+      let info =  transporter.sendMail({
+        from: '"vewww 👻" <vewww.vew@gmail.com >', // sender address
+        to: options.email, // list of receivers
+        subject: "Hello ✔", // Subject line
+        text: "Hello world?", // plain text body
+        html: `
+        <div style="background:#bbf;padding:20px">
+        <h1>${options.message}</h1>
+        <a href="http://localhost:3000/driver/verify/${options.token}">verify</a>
+        </div>
+        `, 
+      },(err,info)=>{
+        if(err){
+            console.log(err);
+        }else{
+            console.log(info);
+        }
+
+      });
+}
--- src/components/MechanicWorkshop/mechanicWorkshop.api.js
+++ src/components/MechanicWorkshop/mechanicWorkshop.api.js
@@ -1,7 +1,9 @@
-const { signup, signin } = require("./mechanicWorkshop.services");
+const { signup, signin, emailVerify } = require("./mechanicWorkshop.services");
 
 const router=require("express").Router()
 
 router.post('/signup', signup)
 router.post('/signin', signin)
+
+router.get('/verify/:token',emailVerify)
 module.exports = router;
