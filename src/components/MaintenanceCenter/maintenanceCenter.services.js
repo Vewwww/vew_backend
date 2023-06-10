c754afe2db0f8b25b4fffd02d00e668a7cfaa139
Rana Gaber <ranagaber810@gmail.com>
2023-06-10 16:01:42 +0300

    updates in maintenance

--- src/components/MaintenanceCenter/maintenanceCenter.services.js
+++ src/components/MaintenanceCenter/maintenanceCenter.services.js
@@ -1,61 +1,80 @@
-const express = require("express");
-const maintanenceCenterModel = require("./maintenanceCenter.model");
-const router = express.Router();
+const maintenanceCenterModel = require("./maintenanceCenter.model");
+const AppErr = require("../../utils/AppErr");
+const { catchAsyncErr } = require("../../utils/CatchAsyncErr");
+const factory=require("../Handlers/handler.factory");
+//create new service
+
+exports.createMaintenanceCenter = factory.createService(maintenanceCenterModel);
 
 
-router.post("/", async (req, res) => {
-    await maintanenceCenterModel.create({
-      name: req.body.name,
-      phoneNumber: req.body.phoneNumber,
-      carType: req.body.carType,
-      isVerified:req.body.isVerified,
-      rate:req.body.rate
-    });
-    res.send({ message: "created" });
+//get all maintenance center
+
+exports.getMaintenanceCenters = catchAsyncErr(async (req, res, next) => {
+  const maintenanceCenters = await maintenanceCenterModel.find();
+  if (!maintenanceCenters) { return next(new AppError("no maintenance center fount", 404)); }
+
+  res.status(200).json({
+    status: "success",
+    results: maintenanceCenters.length,
+    data: maintenanceCenters,
   });
+});
 
-  router.get("/", async (req, res) => {
-    try {
-      res.send(await maintanenceCenterModel.find());
-    } catch (error) {
-      res.status(404).send({ message: "not found" });
-    }
+
+
+
+
+
+//get specific maintenance center with id
+
+exports.getMaintenanceCenter = catchAsyncErr(async (req, res, next) => {
+  const { id } = req.params;
+  const maintenanceCenter = await maintenanceCenterModel.findById(id);
+  if (!maintenanceCenter) {
+    return next(new AppError("No maintenance center found for this id", 404));
+  }
+  res.status(200).json({
+    status: "success",
+    data: maintenanceCenter,
   });
+});
 
-  router.get("/one", async (req, res) => {
-    try {
-      maintanenceName= req.body.name,
-      res.send(await maintanenceCenterModel.findOne({name:maintanenceName}));
-    } catch (error) {
-      res.status(404).send({ message: "not found" });
-    }
+
+
+// update specific maintenance center with id
+exports.updateMaintenanceCenter = catchAsyncErr(async (req, res, next) => {
+  const { id } = req.params;
+  const maintenanceCenter = req.body;
+  const updatedMaintenanceCenter = await maintenanceCenterModel.findOneAndUpdate({ _id: id }, maintenanceCenter, {
+    new: true,
   });
+  if (!maintenanceCenter) {
+    return next(new AppError("No maintenance center found for this id", 404));
+  }
 
-  router.patch("/", async (req, res) => {
-    try {
-      newId=req.body.id
-      newName = req.body.name;
-      newphoneNumber=req.body.phoneNumber;
-      newCarType=req.body.carType,
-      newIsVerified=req.body.isVerified,
-      newRate=req.body.rate
-      await maintanenceCenterModel.findByIdAndUpdate(newId,{name: newName, phoneNumber: newphoneNumber,
-        carType:newCarType,isVerified:newIsVerified ,rate:newRate});
-      res.send({ message: "updated" });
-    } catch (error) {
-      res.status(404).send({ message: "is not updated" });
-    }
+  res.status(201).json({
+    status: "success",
+    data: updatedMaintenanceCenter,
   });
-  
+});
 
-  router.delete("/", async (req, res) => {
-    try {
-      name = req.body.name;
-      await maintanenceCenterModel.findOneAndDelete({ name: name });
-      res.send({ message: "deleted" });
-    } catch (error) {
-      res.status(404).send({ message: "is not deleted" });
-    }
-  });
 
-  module.exports=router;
+
+
+
+
+
+// delete specific maintenance center with id
+
+exports.deleteMaintenanceCenter =  catchAsyncErr(async (req, res, next) => {
+  const { id } = req.params;
+  const deletedMaintenanceCenter = await maintenanceCenterModel.findOneAndDelete({ _id: id });
+
+  if (!deletedMaintenanceCenter) {
+    return next(new AppError("No maintenance center found for this id", 404));
+  }
+
+  res.status(204).send();
+});
+
+
