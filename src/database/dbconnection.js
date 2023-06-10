8c88d739c70ace4382983e27e89d0d8f84a3f4ac
Nourin <nourin.osama55@gmail.com>
2023-06-10 15:47:46 +0300

    updates

--- src/database/dbconnection.js
+++ src/database/dbconnection.js
@@ -1,12 +1,9 @@
-const mongoose = require("mongoose");
+const mongoose=require("mongoose")
 
-exports.dbConnection = () => {
-  mongoose
-    .connect(process.env.CONNECTION_STRING)
-    .then(() => {
-      console.log("database connected");
+exports.dbConnection= ()=>{
+    mongoose.connect(process.env.CONNECTION_STRING).then(()=>{
+        console.log("database connected")
+    }).catch((err)=>{
+        console.log(err)
     })
-    .catch((err) => {
-      console.log(err);
-    });
-};
+}
