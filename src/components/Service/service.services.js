c3333d81bbe536854f092f3511840d3dad594d16
Rana Gaber <ranagaber810@gmail.com>
2023-06-10 16:04:50 +0300

    update services in service

--- src/components/Service/service.services.js
+++ src/components/Service/service.services.js
@@ -1,78 +1,80 @@
-const asyncHandler = require("express-async-handler");
-const service = require("./service.model");
+const serviceModel = require("./service.model");
 const AppErr = require("../../utils/AppErr");
-
+const { catchAsyncErr } = require("../../utils/CatchAsyncErr");
+const factory=require("../Handlers/handler.factory");
 //create new service
 
-exports.createService = asyncHandler(async (req, res) => {
-   const name = req.body;
- 
-    const createdService= await service.create(name);
-  
-    res.status(200).json({
-      status: "success",
-      data: createdService,
-    });
-  });
+exports.createService = factory.createService(serviceModel);
 
 
 //get all service
 
-  exports.getServices = asyncHandler(async (req, res) => {
-    const services = await service.find();
-  
-    res.status(200).json({
-      status: "success",
-      results: services.length,
-      data: services,
-    });
-  });  
+exports.getServices = catchAsyncErr(async (req, res, next) => {
+  const services = await serviceModel.find();
+  if (!services) { return next(new AppError("no services fount", 404)); }
+
+  res.status(200).json({
+    status: "success",
+    results: services.length,
+    data: services,
+  });
+});
+
+
+
 
 
 
 //get specific service with id
 
-  exports.getService = asyncHandler(async (req, res, next) => {
-    const { id } = req.params;
-  
-    const _service = await service.findById(id);
-    if (!_service) {
-      return next(new AppErr("No service found for this id", 404));
-    }
-  
-    res.status(200).json({
-      status: "success",
-      data: _service,
-    });
+exports.getService = catchAsyncErr(async (req, res, next) => {
+  const { id } = req.params;
+  const service = await serviceModel.findById(id);
+  if (!service) {
+    return next(new AppError("No service found for this id", 400));
+  }
+  res.status(200).json({
+    status: "success",
+    data: service,
   });
+});
+
+
 
 // update specific service with id
-  exports.updateService = asyncHandler(async (req, res) => {
-    const { id } = req.params;
-    const _service  = req.body;
-    const updatedService = await service.findOneAndUpdate({ _id: id }, _service, {
-      new: true,
-    });
-  
-    if (!updatedService) {
-      return next(new AppErr("No service found for this id", 404));
-    }
-  
-    res.status(201).json({
-      status: "success",
-      data: updatedService,
-    });
+exports.updateService = catchAsyncErr(async (req, res, next) => {
+  const { id } = req.params;
+  const service = req.body;
+  const updatedService = await serviceModel.findOneAndUpdate({ _id: id }, service, {
+    new: true,
   });
+  if (!service) {
+    return next(new AppError("No service found for this id", 404));
+  }
+
+  res.status(201).json({
+    status: "success",
+    data: updatedService,
+  });
+});
+
+
+
+
+
+
 
 // delete specific service with id
 
-  exports.deleteService = asyncHandler(async (req, res) => {
-    const { id } = req.params;
-    const deletedService = await service.findOneAndDelete({ _id: id });
-  
-    if (!deletedService) {
-      return next(new AppErr("No service found for this id", 404));
-    }
-  
-    res.status(204).send();
-  });
+exports.deleteService =  catchAsyncErr(async (req, res, next) => {
+  const { id } = req.params;
+  const deletedService = await serviceModel.findOneAndDelete({ _id: id });
+
+  if (!deletedService) {
+    return next(new AppError("No service found for this id", 404));
+  }
+
+  res.status(204).send();
+});
+
+
