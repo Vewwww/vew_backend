6797fecb764e312bf76f1a79364fc77c13960cbe
Rana Gaber <ranagaber810@gmail.com>
2023-06-10 18:00:31 +0300

    fixinggg2

--- src/components/winch/winch.validator.js
+++ src/components/winch/winch.validator.js
@@ -5,7 +5,7 @@ const validation = Joi.object({
     ownerName: Joi.string().alphanum().min(3).trim(true).required(),
     email: Joi.string().email().trim(true).required(),
     password: Joi.string().min(6).trim(true).required(),
-    phoneNumber:  Joi.string().length(10).pattern(/^01\d{9}$/).required(),
+    phoneNumber:  Joi.string().length(11).pattern(/^01\d{9}$/).required(),
     plateNumber: Joi.string().alphanum().min(3).trim(true).required(),
 });
 
@@ -17,5 +17,7 @@ winchValidation = async (req, res, next) => {
         console.log(error);
         return next(new AppError("no winch found", 406));
     } 
+    next();
+
 };
 module.exports = winchValidation;
