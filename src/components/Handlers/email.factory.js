c90db08663a3c95bcf642307114b65b2c1c723e7
EmanMK <eman.elokisy@gmail.com>
2023-06-11 18:19:46 +0300

    sign up mechanist-winch with create location, some request services

--- src/components/Handlers/email.factory.js
+++ src/components/Handlers/email.factory.js
@@ -1,27 +1,29 @@
 const nodemailer = require("nodemailer");
 const { catchAsyncErr } = require("../../utils/CatchAsyncErr");
-module.exports.sendEmail=async(options,model)=>{
-    let transporter = nodemailer.createTransport({
-       service:"gmail",
-        auth: {
-          user: "vewww.vew@gmail.com ", // generated ethereal user
-          pass: "cgjnxcjvmxmkuizq", // generated ethereal password
-        },
-      });
-       
-      let modelApiName="driver";
-      if(model.collection.collectionName=== "winches") modelApiName = "winch";
-      if(model.collection.collectionName=== "mechanicworkshops") modelApiName = "mechanic";
+module.exports.sendEmail = async (options, model) => {
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "vewww.vew@gmail.com ", // generated ethereal user
+      pass: "cgjnxcjvmxmkuizq", // generated ethereal password
+    },
+  });
 
-      let info =  transporter.sendMail({
-        from: '"vewww 👻" <vewww.vew@gmail.com >', // sender address
-        to: options.email, // list of receivers
-        subject: "Hello ✔", // Subject line
-        text: "Hello world?", // plain text body
-        html: `
+  let modelApiName = "driver";
+  if (model.collection.collectionName === "winches") modelApiName = "winch";
+  if (model.collection.collectionName === "mechanicworkshops")
+    modelApiName = "mechanic";
+
+  let info = transporter.sendMail(
+    {
+      from: '"vewww 👻" <vewww.vew@gmail.com >', // sender address
+      to: options.email, // list of receivers
+      subject: "Hello ✔", // Subject line
+      text: "Hello world?", // plain text body
+      html: `
         <div style="background:#bbf;padding:20px">
         <h1>${options.message}</h1>
-        <a href="http://localhost:3000/${modelApiName}/verify/${options.token}">verify</a>
+        <a href="${process.env.BaseUrl}/${modelApiName}/verify/${options.token}">verify</a>
         </div>
         `,
     },
