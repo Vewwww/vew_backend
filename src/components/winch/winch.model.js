0d2a1f0c87eb1420afe4f524c1d5474226f8e469
Rana Gaber <ranagaber810@gmail.com>
2023-06-11 19:47:46 +0300

    update in winch validation

--- src/components/winch/winch.model.js
+++ src/components/winch/winch.model.js
@@ -20,9 +20,21 @@ const schema = new mongoose.Schema({
     type: String,
     require: [true, "phone number is required"],
   },
+  car:{
+    carType: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'carType',
+      required: true
+  },
+  color: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'color',
+      required: true
+  },
   plateNumber: {
-    type: String,
-    require: [true, "plate number is required"]
+      type: String,
+      required: true
+  }
   },
   report: {
     reportsNumber: {
