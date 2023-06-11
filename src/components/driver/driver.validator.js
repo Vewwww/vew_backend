8158b2bd8dbd4eefb6dab36cc60e37125aca3664
Rana Gaber <ranagaber810@gmail.com>
2023-06-11 21:20:15 +0300

    update in driver validation

--- src/components/driver/driver.validator.js
+++ src/components/driver/driver.validator.js
@@ -2,16 +2,27 @@ const Joi = require("joi");
 const AppError = require("../../utils/AppError");
 
 const validation = Joi.object({
-  name: Joi.string().alphanum().min(3).trim(true).required(),
+  name: Joi.string().alphanum().min(3).max(15).trim(true).required(),
   email: Joi.string().email().trim(true).required(),
+  changedPasswordAt:Joi.date(),
   password: Joi.string().min(6).trim(true).required(),
   phoneNumber: Joi.string()
     .length(11)
-    .pattern(/^01\d{9}$/)
+    .pattern(/^01\d{9}$/).trim()
     .required(),
   gender: Joi.string().required().valid("male", "female"),
   role: Joi.string().valid("admin", "user").default("user"),
-  lisenceRenewalDate: Joi.date()
+  cars: Joi.array().items(
+    Joi.object({
+      carType: Joi.string().required(),
+      color: Joi.string().required(),
+      plateNumber: Joi.string().required()
+    })).required(),
+  numOfReports:Joi.number().default(0),
+  lisenceRenewalDate: Joi.date(),
+  isSuspended:Joi.boolean().default(false),
+  emailConfirm:Joi.boolean().default(false),
+  logedIn:Joi.boolean().default(false),
   
 });
 
@@ -21,7 +32,7 @@ driverValidation = async (req, res, next) => {
   const { error } = validation.validate(obj);
   if (error) {
     console.log(error);
-    return next(new AppError("no user found", 406));
+    return next(new AppError(error.details[0].message, 406));
   }
   next();
 };
