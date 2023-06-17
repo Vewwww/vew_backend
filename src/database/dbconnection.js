b6442b715358362f65cf9591de4a94247aa109bc
Nourin <nourin.osama55@gmail.com>
2023-06-18 02:20:33 +0300

    multer sharp signsImages

--- src/database/dbconnection.js
+++ src/database/dbconnection.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose")
-require("dotenv").config({ path: "./config/.env" });
+
 exports.dbConnection= ()=>{
     mongoose.connect(process.env.CONNECTION_STRING).then(()=>{
         console.log("database connected")
