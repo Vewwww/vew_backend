79ca10c0934ef7e4382caf470915e65ae8d45c9e
Rana Gaber <ranagaber810@gmail.com>
2023-06-28 19:29:13 +0300

    update sign test

--- src/components/Sign/sign.test.js
+++ src/components/Sign/sign.test.js
@@ -1,18 +1,17 @@
 const request = require("supertest");
 const baseURL = "http://localhost:3000"
-
+//const app=require("../../../app")
 describe("get signs", () => {
     test("get driver pending request", async () => {
         const res = await request(baseURL).get("/sign/");
-        res.body.data.forEach(element => {
-            // if (element.name && element.image && element.description && element.solution) {
 
-            //     ex
-            // }
+        // const res = await request(app).get("/sign/");
+        res.body.data.forEach(element => {
+
 
             expect(element.name).toBeDefined()
             expect(element.description).toBeDefined()
-            // expect(element.solution).toBeDefined()
+            expect(element.solution).toBeDefined()
             expect(element.image).toBeDefined()
 
         });
