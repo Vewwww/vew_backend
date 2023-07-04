596f3c1f91af990dfd893dbfffb9690c5425b4eb
Rana Gaber <ranagaber810@gmail.com>
2023-07-04 10:00:15 +0300

    get all categories&&questionsby category

--- src/components/CBR/Question/question.api.js
+++ src/components/CBR/Question/question.api.js
@@ -5,11 +5,14 @@ const {
     getQuestion,
     updateQuestion,
     deleteQuestion,
+    getAllCategories,
+    getQuestionsByCategory
 } = require("./question.services");
 const questionValidation = require("./question.validator");
 
 const router = express.Router();
-
+router.route("/category/:category").get(getQuestionsByCategory);
+router.route("/getAllCategories").get(getAllCategories);
 router.route("/").post(questionValidation, createQuestion).get(getQuestions);
 router.route("/:id").get(getQuestion).put(updateQuestion).delete(deleteQuestion);
 
--- src/components/CBR/Question/question.services.js
+++ src/components/CBR/Question/question.services.js
@@ -13,6 +13,32 @@ exports.getQuestion = factory.getOne(questionModel);
 
 exports.updateQuestion = factory.updateOne(questionModel);
 
+exports.getAllCategories = catchAsyncErr(async (req, res, next) => {
+    const categories = await questionModel.distinct("category");
+
+    console.log(categories);
+    if (!categories) {
+        return next(new AppError("No categories found", 404));
+    }
+    res.status(200).json({
+        data: categories
+    });
+});
+
+
+exports.getQuestionsByCategory = catchAsyncErr(async (req, res, next) => {
+    const { category } = req.params;
+
+    const questions = await questionModel.find({ category }).select("questions");
+
+    console.log(questions);
+    if (questions.length == 0) {
+        return next(new AppError(`No question found for "${category}"`, 404));
+    }
+    res.status(200).json({
+        data: questions
+    });
+});
 
 
 exports.deleteQuestion = catchAsyncErr(async (req, res, next) => {
